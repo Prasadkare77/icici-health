6467bc430e416e8394e294ddf03a38ee3beaaab6
Prasadkare77 <prasadkare77@gmail.com>
2024-02-28 22:42:18 +0530

    Tabs

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,12 +6,21 @@ const Home = () => {
   return (
     <div>
       <div className='background'>
-        <div className='main-banner-bg w-full h-full' style={{ backgroundImage: `url(${MainBannerBg})` }}>
-          <div className='inline-block left-0'>
-            <h2 className='mt-5 font-extrabold text-lg'>Over 4 crore customers have faith in<br/> us because we care.</h2>
+        <div className='main-banner-bg flex w-full' style={{ backgroundImage: `url(${MainBannerBg})` }}>
+          <div className='inline-block text-left mt-10 ms-10'>
+            <h2 className='mt-5 font-bold text-3xl text-left'>Over 4 crore customers have faith in<br/> us because we care.</h2>
           </div>
-          <div className='inline-block'>
+          <div className='inline-block right-0 ms-60'>
             <img src={BannerImg} alt="" />
+          </div>
+        </div>
+
+        <div>
+          <div className='tabs'>
+            
+          </div>
+          <div className='carasole'>
+            
           </div>
         </div>
       </div>
