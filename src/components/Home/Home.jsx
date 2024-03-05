2919f3e15b227986450a40c3fce87cea777ebae6
Prasadkare77 <prasadkare77@gmail.com>
2024-03-05 23:27:31 +0530

    Tabs

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,9 +11,14 @@ import Business from '../../assets/Home/Tabs/business.png'
 import Renewal from '../../assets/Home/Tabs/renewal.png'
 import { Button } from 'bootstrap';
 
+import { Select, Option } from "@material-tailwind/react";
+
 
 const Home = () => {
   const [active, setActive] = useState("car");
+
+  
+
   const data = [
     {
       label: "Car",
@@ -93,6 +98,44 @@ const Home = () => {
       label: "Health",
       value: "health",
       icon: <img src={Health} />,
+      desc: <div className="flex flex-row gap-6 w-auto h-28">
+              <div className="relative h-11 w-full min-w-[200px]">
+                <Select variant="standard" label="">
+                  <Option>Max Protect</Option>
+                  <Option>Health AdvantEdge</Option>
+                  <Option>Aarogya Sanjeevani Policy, ICICI Lombard</Option>
+                  <Option>Health Booster</Option>
+                  <Option>Personal Protect</Option>
+                </Select>
+                  <label
+                    className="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    Select Products
+                  </label>
+                
+              </div>
+              <div className="relative h-11 w-full min-w-[200px]">
+                <input placeholder="Enter Mobile No"
+                  className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
+                <label
+                  className="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-black transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                  Mobile Number<span><sup className='text-red-500'>*</sup></span>
+                </label>
+              </div>
+              <div className="relative h-11 w-full min-w-[200px]">
+                <input placeholder="Enter Email Address"
+                  className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
+                <label
+                  className="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-black transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                  Email<span><sup className='text-red-500'>*</sup></span>
+                </label>
+              </div>
+              <div className="relative h-11 w-full min-w-[200px]">
+                <button className="block bg-orange-500 hover:bg-orange-700 text-white font-semibold py-3 px-6 ms-9 rounded-full">
+                  Get qoute
+                </button>
+                <a href="" className='underline block text-orange-500 text-center mt-3'>Recent Quote</a>
+              </div>
+            </div>
     },
     {
       label: "Travel",
