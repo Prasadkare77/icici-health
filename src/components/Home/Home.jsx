a1a7cecd1cb2f0be0c8d2bb5ad8290e17bdb408c
Prasadkare77 <prasadkare77@gmail.com>
2024-03-07 23:10:21 +0530

    Tab changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -159,8 +159,9 @@ const Home = () => {
                       <div className="relative h-11 w-full min-w-[200px]">
                         <input placeholder="Mobile No"
                           class="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                          value={formData.mobileNo}
-                          onChange={handleChange} />
+                          // value={formData.mobileNo}
+                          onChange={handleChange}
+                          />
                         <label
                           class="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                           Mobile No.<span><sup className='text-red-500'>*</sup></span>
@@ -169,8 +170,9 @@ const Home = () => {
                       <div className="relative h-11 w-full min-w-[200px]">
                         <input placeholder="Enter Email Address"
                             class="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            value={formData.email}
-                            onChange={handleChange} />
+                            // value={formData.email}
+                            onChange={handleChange}
+                            />
                           <label
                             class="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Email.<span><sup className='text-red-500'>*</sup></span>
@@ -179,8 +181,9 @@ const Home = () => {
                       <div className="relative h-11 w-full min-w-[200px]">
                         <input placeholder="Enter Pincode"
                             class="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            value={formData.pincode}
-                            onChange={handleChange} />
+                            // value={formData.pincode}
+                            onChange={handleChange} 
+                            />
                           <label
                             class="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Pincode.<span><sup className='text-red-500'>*</sup></span>
@@ -189,8 +192,9 @@ const Home = () => {
                       <div className="relative h-11 w-full min-w-[200px]">
                         <input placeholder="Enter Your Name"
                             class="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            value={formData.yourName}
-                            onChange={handleChange} />
+                            // value={formData.yourName}
+                            onChange={handleChange} 
+                            />
                           <label
                             class="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Your Name.
