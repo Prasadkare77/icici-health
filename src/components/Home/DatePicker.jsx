a8bef3976575bf1b359e806924a80d187059ed3e
Prasadkare77 <prasadkare77@gmail.com>
2024-07-05 20:09:18 +0530

    React Tailwindcss Datepicker uses Tailwind CSS 3
    React Tailwind css Datepicker uses Tailwind CSS 3

---
+++ src/components/Home/DatePicker.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Datepicker from 'react-tailwindcss-datepicker'
+
+const DatePicker = () => {
+    const [value, setValue] = useState({
+        startDate: new Date(),
+        endDate: new Date().setMonth(11)
+    })
+
+    const handleValueChange = (newValue) => {
+        console.log(`Newvalue: ${newValue}`)
+        setValue(newValue);
+    }
+
+  return (
+    <div>
+        <Datepicker 
+            value={value} 
+            onChange={handleValueChange} 
+        /> 
+    </div>
+  )
+}
+
+export default DatePicker
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,6 +13,7 @@ import { Button } from 'bootstrap';
 
 import { Select, Option } from "@material-tailwind/react";
 import { Carousel } from "@material-tailwind/react";
+import DatePicker from './DatePicker';
 
 
 const Home = () => {
@@ -254,20 +255,14 @@ const Home = () => {
               </div>
 
               {/* Flowbite Tailwind CSS CDN  Link for Date Picker */}
+              <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
               <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
 
               <div className="relative h-11 w-full min-w-[200px]">
                 <select  className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
-                      <option >
-                        <div className="relative max-w-sm">
-                          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                            </svg>
-                          </div>
-                          <input datepicker id="default-datepicker" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
-                        </div>
-                      </option>
+                    <option >
+                      <DatePicker />
+                    </option>
                   </select>
                   <label
                     className="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
