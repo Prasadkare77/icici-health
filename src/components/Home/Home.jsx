7ac06d3915efe6826793205a1b16ae97196cf0b6
Prasadkare77 <prasadkare77@gmail.com>
2024-07-05 19:26:36 +0530

    Home Updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -239,6 +239,7 @@ const Home = () => {
                   Your travel scope
                 </label>
               </div>
+              
               <div className="relative h-11 w-full min-w-[200px]">
                 <select value={dropdown1} onChange={(e) => setDropdown1(e.target.value)} className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
                     {travelScopeOptons.map((option) => (
@@ -308,6 +309,7 @@ const Home = () => {
                       </div>
                     </form>
                   )}
+
                 <label
                   className="after:content[' '] pointer-events-none absolute left-0  -top-2.5 flex h-full w-full select-none !overflow-visible truncate text-sm font-normal leading-tight text-black transition-all after:absolute after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                   Contact Details<span><sup className='text-red-500'>*</sup></span>
